2f74ffbf289fe63a64af0b515a25fac771b6124f
Alex-Polishchuk <alexpoli147@gmail.com>
2024-01-03 21:03:20 +0000

    More JS practice

--- attempt_1.js
+++ attempt_1.js
@@ -93,3 +93,132 @@ function myOutfit() {
 
 console.log(myOutfit());
 console.log(outerWear);
+
+function NextInLine(arr, item) {
+
+    arr.push(item)
+    return arr.shift();
+}
+arr = [1, 2, 3, 4, 5]
+console.log(arr)
+NextInLine(arr, 6)
+console.log(arr)
+
+/* Bool */
+
+function TrueOrFalse (wasThatTrue) {
+    if (wasThatTrue) {
+        return "Yes That's true";
+    }
+   return "Oh no that's false";
+}
+
+console.log(TrueOrFalse(true));
+
+function CompareEqual (a, b){
+    if (a===b){
+        return true;
+    }
+    return false;
+}
+
+console.log(CompareEqual(5, '5'));
+console.log(CompareEqual(5, 5));
+
+/* this mean && and*/
+
+function testElseIf (val) {
+    if (val > 10) {
+        return "Greater than 10"
+    }
+    else if (val < 5) {
+        return "Less than 5"
+    }
+    else {
+        return "Between 5 & 10"
+    }
+}
+
+console.log(testElseIf(7))
+
+/* Switch operator in JS*/
+function CaseInSwitch (num) {
+    var answer;
+    switch(num) {
+        case 1:
+            answer = "alpha";
+            break
+        
+        case 2:
+            answer = "beta";
+            break
+
+        case 3:
+            answer = "gamma";
+            break
+
+        case 4:
+            answer = "delta";
+            break
+    }
+
+    return answer;
+}
+
+console.log(CaseInSwitch(2))
+
+function SwitchOfStuff (input) {
+
+    let final;
+
+    switch(input) {
+        case a:
+            final = "Apple";
+            break
+        
+        case b:
+            final = "Banana";
+            break
+        
+        case c:
+            final = "Cumquat";
+            break
+
+        case d:
+            final = "Dried Fruit";
+            break
+        
+        default:
+            final = "No"
+            break
+    }
+
+    return final
+}
+
+/* omit the break statement if you wish to have multiple outputs from a switch statements */
+
+function HighOrLow (val) {
+    let ans;
+
+    switch(val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low"
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid"
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High"
+            break;
+    }
+    return answer
+}
+
+console.log(HighOrLow(5))
